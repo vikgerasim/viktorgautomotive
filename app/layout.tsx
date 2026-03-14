9114e175e941bd932be2ad83d13f1f90046a8e91
vikgerasim <gerasimenkos@shaw.ca>
2026-03-13 22:14:59 -0600

    add Google Analytics and Search Console verification

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import Link from "next/link";
 import SearchBar from "@/components/SearchBar";
 import { buildSearchIndex } from "@/lib/search";
 import Footer from "@/components/Footer";
+import Script from "next/script";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -44,6 +45,18 @@ export default function RootLayout({
           </div>
         </header>
         {children}
+        <Script
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}');
+          `}
+        </Script>
         <Footer />
       </body>
     </html>
